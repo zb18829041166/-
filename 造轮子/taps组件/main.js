239287cffbb937a59ab00dd476b62e06d84c484b
zb <2479107942@qq.com>
2018-05-25 16:53:41 +0800

    本次使用了class语法，需要注意new的用法，以及constructor的用法

---
+++ 造轮子/taps组件/main.js
@@ -0,0 +1,30 @@
+class Tabs{
+    constructor(selector){
+        this.elements=$(selector)
+        this.init()
+        this.bindEvent()
+    }
+
+    init() {
+        this.elements.each((index,element) => {
+            $(element).children('.tab-bar').children('li').eq(0).addClass('active')
+            $(element).children('.tab-content').children('li').eq(0).addClass('active')
+        });
+    }
+
+    bindEvent(){
+        this.elements.on('click', '.tab-bar > li', function(e){
+            var $e=$(e.currentTarget)
+            $e.addClass('active').siblings().removeClass('active')
+            var index=$e.index()
+            var $content=$e.closest('.tabs').find('.tab-content').children('li').eq(index)
+            $content.addClass('active').siblings().removeClass('active')
+        });
+    }
+}
+
+
+
+
+
+var tabs = new Tabs('.xxx')    
